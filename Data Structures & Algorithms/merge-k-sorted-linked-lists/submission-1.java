e531e0380cf8e120caf165354ede5e1dbbf74b25
Rekhit Gupta <officialrek.07@gmail.com>
2026-05-23 19:18:14 +0530

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.java
@@ -0,0 +1,96 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0){
+            return null;
+        }
+        if(lists.length == 1){
+            return lists[0];
+        }
+        int n = lists.length-1;
+        ListNode result = divide(lists, 0, n);
+        // ListNode llr= lists[0];
+        // int i=1;
+        // while(lists.length > i){
+        //     ListNode llt = lists[i];
+        //     ListNode lltr = mergeTwoLL(llr, llt);
+        //     llr = lltr;
+        //     i++;
+        // }
+        // return llr;
+        return result;
+    }
+
+    public ListNode divide(ListNode[] lists, int i, int j){
+        if(i==j){
+            return lists[i];
+        }
+        if(i>j){
+            return null;
+        }
+        int m = i + (j-i)/2;
+        ListNode leftresult = divide(lists, i, m);
+        ListNode rightresult = divide(lists, m+1, j);
+        return mergeTwoLL(leftresult, rightresult);
+    }
+
+    public ListNode mergeTwoLL(ListNode h1, ListNode h2){
+        ListNode temphead = null;
+        ListNode tempcurr = null;
+        while(h1!=null && h2!=null){
+            
+            if(h1.val<h2.val){
+                if(temphead==null){
+                    temphead = h1;
+                    tempcurr = h1;
+                }
+                else{
+                    tempcurr.next = h1;
+                    tempcurr = tempcurr.next; 
+                }
+                h1 = h1.next;
+            }
+            else{
+                if(temphead==null){
+                    temphead = h2;
+                    tempcurr = h2;
+                }
+                else{
+                    tempcurr.next = h2;
+                    tempcurr = tempcurr.next; 
+                }
+                h2 = h2.next;
+            }
+        }
+
+        if(h1!=null){
+            if(temphead==null){
+                temphead = h1;
+                tempcurr = h1;
+            }
+            else{
+                tempcurr.next = h1;
+            }
+        }
+
+        if(h2!=null){
+            if(temphead==null){
+                temphead = h2;
+                tempcurr = h2;
+            }
+            else{
+                tempcurr.next = h2;
+            }
+        }
+        return temphead;
+    }
+}
